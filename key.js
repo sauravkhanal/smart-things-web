83dc7aa796429960057b233e09a2b045c1d4468f
sauravkhanal <sauravkhanal635@gmail.com>
2025-04-13 21:19:27 +0545

    Add new token, token had expired

--- key.js
+++ key.js
@@ -1,2 +1,2 @@
-export const DEVICE_KEY = "ba156bea-f254-4535-b55d-7b0099b1da4c";
+export const DEVICE_KEY = "7e6c5d0e-1764-4a7a-95c8-e0ec4434b3b9";
 export const DEVICE_ID = "b4471eef-de28-313a-6799-12bfafdfd9b4";
